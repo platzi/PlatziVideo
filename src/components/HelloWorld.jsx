efc031da63baab8f0be03460a950b7fc06dcd9ce
Oscar Barajas <oscar@dragons.mx>
2019-07-09 18:16:18 -0500

    sass

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../assets/styles/App.scss';
 
 const HelloWorld = () => (
   <h1>Hola Mundo</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -26,6 +27,16 @@ module.exports = {
             loader: 'html-loader'
           }
         ]
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
@@ -33,6 +44,9 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './public/index.html',
       filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css'
     }),
   ]
 };
