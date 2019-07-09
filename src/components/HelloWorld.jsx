d09515dfd28042fa19fb28c36a84119d6fd84717
Oscar Barajas <oscar@dragons.mx>
2019-07-09 17:33:30 -0500

    Webpack-dev-server

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const HelloWorld = () => {
+const HelloWorld = () => (
   <h1>Hola Mundo</h1>
-};
+);
 
 export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
 
-ReactDOM.render(<HelloWorld />, document.getElementById(app));
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
