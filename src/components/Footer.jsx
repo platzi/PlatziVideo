a39da5edbe8d56f4b35da59973c99cb36c6fefe8
Oscar Barajas <oscar@dragons.mx>
2019-07-10 16:01:07 -0500

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/components/Footer.scss';
+
+const Footer = () => (
+  <footer className="footer">
+    <a href="/">Terminos de uso</a>
+    <a href="/">Declaración de privacidad</a>
+    <a href="/">Centro de ayuda</a>
+  </footer>
+);
+
+export default Footer;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
 
@@ -21,6 +22,7 @@ const App = () => (
       </Carousel>
     </Categories>
 
+    <Footer />
   </div>
 );
 
