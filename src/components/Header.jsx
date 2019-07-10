5821e0f8ecf8a165dd8787cb1e666b780abc64d4
Oscar Barajas <oscar@dragons.mx>
2019-07-10 15:20:06 -0500

    Header Styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../assets/styles/components/Header.scss';
 
 const Header = () => (
   <header className="header">
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import '../assets/styles/App.scss';
 
 const App = () => (
   <div className="App">
