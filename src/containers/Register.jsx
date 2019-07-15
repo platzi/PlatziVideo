243c6239846f003ca26b369e7e1101a2c408f08d
Oscar Barajas <oscar@dragons.mx>
2019-07-15 10:50:48 -0500

    [ADD] Register

---
+++ src/containers/Register.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../assets/styles/components/Register.scss';
+
+const Register = () => (
+  <section className="register">
+    <section className="register__container">
+      <h2>Regístrate</h2>
+      <form className="register__container--form">
+        <input className="input" type="text" placeholder="Nombre" />
+        <input className="input" type="text" placeholder="Correo" />
+        <input className="input" type="password" placeholder="Contraseña" />
+        <button className="button">Registrarme</button>
+      </form>
+      <a href="">Iniciar sesión</a>
+    </section>
+  </section>
+);
+
+export default Register;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
+import Register from '../containers/Register';
 
 const App = () => (
   <BrowserRouter>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/login" component={Login} />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
   </BrowserRouter>
 )
 
