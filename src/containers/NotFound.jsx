b4175877668c39217770d2019299355dde8e2d13
Oscar Barajas <oscar@dragons.mx>
2019-07-15 11:09:12 -0500

    [ADD] NotFound

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <>
+    <h1>No Encontrado</h1>
+    <h2>Regresa al Home</h2>
+  </>
+);
+
+export default NotFound;
--- src/routes/App.js
+++ src/routes/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
 import Register from '../containers/Register';
+import NotFound from '../containers/NotFound';
 
 const App = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 )
