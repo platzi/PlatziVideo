26a9d65a0060906ccc23b83f3f0879fd5ebf0382
Oscar Barajas <oscar@dragons.mx>
2019-07-18 09:10:23 -0500

    [ADD] Player react-router

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import '../assets/styles/components/Player.scss';
 
-const Player = () => {
+const Player = props => {
+  const { id } = props.match.params;
   return (
     <div className="Player">
       <video controls autoPlay>
         <source src="" type="video/mp4" />
       </video>
-      <div className="Player-black">
-        <button type="button">
+      <div className="Player-back">
+        <button type="button" onClick={() => props.history.goBack()}>
           Regresar
         </button>
       </div>
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx']
