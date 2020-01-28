75017587da5ca98ccbe2b043897712f98725de13
Carlos Sampol <sampol.90@gmail.com>
2020-01-28 12:09:47 -0500

    Aplicando assets require hook

--- src/server/index.js
+++ src/server/index.js
@@ -4,4 +4,9 @@ require('@babel/register')({
   presets: ['@babel/preset-env', '@babel/preset-react'],
 });
 
+require('asset-require-hook')({
+  extensions: ['jpg', 'png', 'gif'],
+  name: '/assets/[hash].[ext]',
+});
+
 require('./server');
