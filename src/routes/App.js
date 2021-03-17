9fba7c57abbba3030d0abeb239a12ed7477f0ff9
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-17 15:22:33 -0500

    Adding react-router-dom

---
+++ src/routes/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from '../containers/Home';
+
+const App = () => (
+    <BrowserRouter>
+        <Route exact path="/" component={Home}/>
+    </BrowserRouter>
+);
+export default App;
